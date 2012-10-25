755cbde76e2e7016b791e62eb34fa83ab53310bc
tokuhirom <tokuhirom@gmail.com>
2012-10-26 08:52:55 +0900

    initial import

---
+++ lib/Test/Should.pm
@@ -0,0 +1,39 @@
+package Test::Should;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Test::Should - A module for you
+
+=head1 SYNOPSIS
+
+  use Test::Should;
+
+=head1 DESCRIPTION
+
+Test::Should is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
